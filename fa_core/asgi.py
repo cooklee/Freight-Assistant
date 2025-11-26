ae8c779d896204713b486d8ee1ebb77b8d4297dd
Shizol01 <wrzosek-krystian@wp.pl>
2025-11-26 10:59:17 +0100

    chore(settings): switch Django settings to dev environment

--- fa_core/asgi.py
+++ fa_core/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fa_core.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fa_core.settings.dev')
 
 application = get_asgi_application()
