ade1eadafc63fca47c226c5d3ed28815e7d8ed1a
Shizol01 <wrzosek-krystian@wp.pl>
2025-11-27 17:41:50 +0100

    chore(accounts): setup URL routes for auth and profile views

---
+++ apps/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from apps.accounts.views import (
+    LoginView, LogoutView,
+    RegisterView,
+    ProfileView,
+    ProfileImageDeleteView,
+)
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/delete-image/', ProfileImageDeleteView.as_view(), name='profile_delete_image'),
+]
