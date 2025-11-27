6bac14b4038d2f2192a7901b4ddada66898dcd17
Shizol01 <wrzosek-krystian@wp.pl>
2025-11-27 17:38:58 +0100

    chore(accounts): register UserProfile in admin with inline

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import UserProfile
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    fk_name = "user"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile)
