b7d0b0f1830132f25a3e8be1cc14c1bc8bdd6665
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-05 23:19:44 +0100

    chore(company): add urld for company branch crud and list

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -4,7 +4,9 @@ from .views import (CarrierAddView, CarrierDetailView,
                     CarrierUpdateView, CarrierDeleteView,
                     CarrierListView, CustomerAddView, CustomerDetailView,
                     CustomerUpdateView, CustomerDeleteView,
-                    CustomerListView)
+                    CustomerListView, CustomerBranchAddView, CustomerBranchDetailView,
+                    CustomerBranchUpdateView, CustomerBranchDeleteView,
+                    CustomerBranchListView)
 
 urlpatterns = [
     path('carrier/add/', CarrierAddView.as_view(), name='carrier-add'),
@@ -17,4 +19,9 @@ urlpatterns = [
     path('customer/update/<int:customer_id>/', CustomerUpdateView.as_view(), name='customer-update'),
     path('customer/delete/<int:customer_id>/', CustomerDeleteView.as_view(), name='customer-delete'),
     path('customer/all/', CustomerListView.as_view(), name='customer-list'),
+    path('branch/add/<int:customer_id>/', CustomerBranchAddView.as_view(), name='branch-add'),
+    path('branch/detail/<int:branch_id>/', CustomerBranchDetailView.as_view(), name='branch-detail'),
+    path('branch/update/<int:branch_id>/', CustomerBranchUpdateView.as_view(), name='branch-update'),
+    path('branch/delete/<int:branch_id>/', CustomerBranchDeleteView.as_view(), name='branch-delete'),
+    path('branch/all/', CustomerBranchListView.as_view(), name='branch-list'),
 ]
