7ce65035e4673a5035ae76a6764336f7a9ac8afe
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-08 12:53:59 +0100

    feat(drivers): add urls for crud and list for drivers

--- apps/drivers/urls.py
+++ apps/drivers/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import DriverListView, DriverAddView, DriverDetailView, DriverUpdateView, DriverDeleteView
+
+urlpatterns = [
+    path('add/<int:carrier_id>/', DriverAddView.as_view(), name='driver-add'),
+    path('detail/<int:driver_id>/', DriverDetailView.as_view(), name='driver-detail'),
+    path('update/<int:driver_id>/', DriverUpdateView.as_view(), name='driver-update'),
+    path('delete/<int:driver_id>/', DriverDeleteView.as_view(), name='driver-delete'),
+    path('all/', DriverListView.as_view(), name='driver-list'),
+]
