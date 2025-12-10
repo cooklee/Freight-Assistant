f612c89c164ef98e1aabd4fcf7a677145a54df08
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-10 17:42:58 +0100

    refactor(transport): add module initialization

---
+++ apps/transport/forms/__init__.py
@@ -0,0 +1,4 @@
+from .calculation import CalculationForm
+from .route import RouteForm
+from .stop import StopForm, StopFormSet
+from .transport_order import TransportOrderForm, TransportOrderUpdateForm
